890e2383c5c4b2ebfbaf23c5473fe6025a251042
dpslwk <dps.lwk@gmail.com>
2018-10-27 03:29:48 +0100

    Revert temp and light

--- ss_temperature.php
+++ ss_temperature.php
@@ -1,33 +1,40 @@
 <?php
-require "/home/cacti/www_secure/db.php";
+	require "/home/cacti/www_secure/db.php";
 
-/* do NOT run this script through a web browser */
-if (!isset($_SERVER["argv"][0]) || isset($_SERVER['REQUEST_METHOD'])  || isset($_SERVER['REMOTE_ADDR'])) {
-	die("<br><strong>This script is only meant to run at the command line.</strong>");
-}
-
-$no_http_headers = true;
-
-/* display ALL errors */
-error_reporting(0);
-
-if (!isset($called_by_script_server)) {
-	include_once(dirname(__FILE__) . "/../include/global.php");
+	/* do NOT run this script through a web browser */
+	if (!isset($_SERVER["argv"][0]) || isset($_SERVER['REQUEST_METHOD'])  || isset($_SERVER['REMOTE_ADDR'])) {
+		die("<br><strong>This script is only meant to run at the command line.</strong>");
+	}
 	
-	print call_user_func("ss_temperature");
-}
+	$no_http_headers = true;
+	
+	/* display ALL errors */
+	error_reporting(0);
+	
+	if (!isset($called_by_script_server)) {
+		include_once(dirname(__FILE__) . "/../include/global.php");
+		
+		print call_user_func("ss_temperature");
+	}
+	
+	function ss_temperature() {
+//		$db_host = "localhost";
+//		$db_user = "gk";
+//		$db_pass = "gk";
+//		$db_db = "instrumentation";
 
-function ss_temperature() {
-	$idb = db_link();
-	$query = "SELECT name, temperature FROM temperature WHERE name IS NOT NULL ";
 
-	$result = $idb->query($query);
-	$output = "";
+//		$idb = new mysqli($db_host, $db_user, $db_pass, $db_db);
+		$idb = db_link();
+		$query = "SELECT name, temperature FROM temperature WHERE name IS NOT NULL ";
 
-	while ($row = $result->fetch_object())
-		$output .= $row->name.':'.$row->temperature.' ';
+		$result = $idb->query($query);
+		$output = "";
 
-	return $output;
-}
+		while ($row = $result->fetch_object())
+			$output .= $row->name.':'.$row->temperature.' ';
+
+		return $output;
+	}
 
 ?>
